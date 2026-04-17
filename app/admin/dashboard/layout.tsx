dc619dca800e1a9d7bc9d33ddfa2f317dc127d09
hashspider26 <thehashspider@gmail.com>
2026-04-17 22:04:33 +0500

    .

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -3,6 +3,8 @@ import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { AdminTabs } from "@/components/admin/admin-tabs";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
     children,
 }: {
